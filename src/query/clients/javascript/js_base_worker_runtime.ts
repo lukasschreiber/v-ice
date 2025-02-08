f031d7433a824631d49c9c42f1c7cf3148a2f653
lukasschreiber <info@lukasschreiber.com>
2025-02-08 18:53:07 +0100

    base worker

---
+++ src/query/clients/javascript/js_base_worker_runtime.ts
@@ -0,0 +1,38 @@
+import { DataTable } from "@/data/table";
+import { LocalQueryRuntime, QueryFnReturnType } from "../local_query_runtime";
+import { subscribe } from "@/store/subscribe";
+import { FilteredDataTable } from "@/data/filtered_table";
+import { QueryWorkerInterface } from "@/query/workers/query_worker_interface";
+
+export class JSBaseWorkerRuntime extends LocalQueryRuntime {
+    private worker: QueryWorkerInterface;
+
+    constructor(worker: Worker) {
+        super();
+        this.worker = new QueryWorkerInterface(worker);
+    }
+
+    public initialize(): Promise<void> {
+        subscribe((state) => state.sourceTable, (value) => {
+            this.source = value;
+            this.worker.setRows(value.rows);
+        });
+
+        return Promise.resolve();
+    }
+
+    execute(query: string): Promise<QueryFnReturnType<FilteredDataTable>> {
+        return new Promise((resolve) => {
+            if (!this.worker || !this.source || this.source.columns.length === 0 || this.source.rows.length === 0 || query === "") {
+                resolve({ targets: {}, edgeCounts: {} });
+                return;
+            }
+
+            const table = DataTable.fromNormalizedTable(this.source);
+
+            this.worker.runQuery(table, query).then((result) => {
+                resolve(result);
+            });
+        });
+    }
+}
--- src/query/clients/javascript/js_secure_runtime.ts
+++ src/query/clients/javascript/js_secure_runtime.ts
@@ -1,35 +1,8 @@
-import { DataTable } from "@/data/table";
-import { LocalQueryRuntime, QueryFnReturnType } from "../local_query_runtime";
 import SecureQueryWorker from "@/query/workers/js_secure_query_worker?worker";
-import { subscribe } from "@/store/subscribe";
-import { FilteredDataTable } from "@/data/filtered_table";
-import { QueryWorkerInterface } from "@/query/workers/query_worker_interface";
+import { JSBaseWorkerRuntime } from "./js_base_worker_runtime";
 
-export class JSSecureRuntime extends LocalQueryRuntime {
-    private worker: QueryWorkerInterface | null = null;
-
-    public initialize(): Promise<void> {
-        this.worker = new QueryWorkerInterface(new SecureQueryWorker());
-        subscribe((state) => state.sourceTable, (value) => {
-            this.source = value;
-            this.worker?.setRows(value.rows);
-        });
-
-        return Promise.resolve();
-    }
-
-    execute(query: string): Promise<QueryFnReturnType<FilteredDataTable>> {
-        return new Promise((resolve) => {
-            if (!this.worker || !this.source || this.source.columns.length === 0 || this.source.rows.length === 0 || query === "") {
-                resolve({ targets: {}, edgeCounts: {} });
-                return;
-            }
-
-            const table = DataTable.fromNormalizedTable(this.source);
-
-            this.worker.runQuery(table, query).then((result) => {
-                resolve(result);
-            });
-        });
+export class JSSecureRuntime extends JSBaseWorkerRuntime {
+    constructor() {
+        super(new SecureQueryWorker());
     }
 }
